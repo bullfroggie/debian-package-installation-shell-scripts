73aba0fdaec0d87166dbe475ce58a060defbf424
Nikola Lausev <lausevnikola@protonmail.com>
2020-01-19 13:39:46 +0000

    fix typo

--- install_discord.sh
+++ install_discord.sh
@@ -3,4 +3,4 @@
 wget "https://discordapp.com/api/download?platform=linux&format=deb" ~/Downloads
 sudo dpkg --install ~/Downloads/discord-*.*.*.deb
 rm ~/Downloads/discord-*.*.*.deb
-echo "Discord succesfully installed!"
+echo "Discord successfully installed!"
